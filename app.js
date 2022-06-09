05e1c0f197cb425ce123117e91ef8aeba67e091a
DisturbedMindSet <67000500+DisturbedMindSet@users.noreply.github.com>
2022-06-09 17:28:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
 const app = express();
 
 const port = process.env.port || 4000;
+
+const http = require("http");
+const server = http.server(app);
 
 app.get("/", (req, res) => {
 	res.sendFile("/index.html", { root: __dirname });
